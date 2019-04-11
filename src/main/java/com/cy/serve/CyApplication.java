391e09f6c3f751d04e5bc352cad7e4be2cbc6976
Administrator <1186389154@qq.com>
2019-04-11 22:42:24 +0800

    123

---
+++ src/main/java/com/cy/serve/CyApplication.java
@@ -0,0 +1,13 @@
+package com.cy.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cy/serve/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.cy.serve.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class UserController {
+
+    @PostMapping("/login")
+    public String login(){
+
+
+    }
+}
